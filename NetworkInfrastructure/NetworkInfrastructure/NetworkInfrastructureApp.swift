4414fa03735139fed6763200925cb6208e23f6e6
Donguk Kim <ehddnr73@naver.com>
2023-03-01 20:14:33 +0900

    NetworkInfrastructure 프로젝트 생성

---
+++ NetworkInfrastructure/NetworkInfrastructure/NetworkInfrastructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkInfrastructureApp.swift
+//  NetworkInfrastructure
+//
+//  Created by 김동욱 on 2023/03/01.
+//
+
+import SwiftUI
+
+@main
+struct NetworkInfrastructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
