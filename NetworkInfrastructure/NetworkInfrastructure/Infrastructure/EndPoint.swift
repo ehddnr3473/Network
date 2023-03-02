8ba081e4b46cb97e19be94cdd4a37fa5d18ba60c
Donguk Kim <ehddnr73@naver.com>
2023-03-02 16:06:18 +0900

    enum EndPoint 정의

--- NetworkInfrastructure/NetworkInfrastructure/Infrastructure/EndPoint.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Infrastructure/EndPoint.swift
@@ -23,3 +23,30 @@ public enum HttpMethodType: String {
     case head   = "HEAD"
     case patch  = "PATCH"
 }
+
+
+/// EndPoint
+/// REST API에서 클라이언트가 서버의 특정 기능을 호출하기 위해 필요한 URI의 일부분
+/// API에서 제공하는 각각의 기능이나 자원에 대한 경로(여기에서는 URL)
+/// EndPoint는 HTTP 메서드와 함께 사용되어, 클라이언트가 서버에 요청을 보낼 때
+/// 어떤 EndPoint에 어떤 HTTP 메서드를 사용해야 하는지를 결정.
+/// path + query item
+public enum EndPoint {
+    case `default`
+}
+
+public extension EndPoint {
+    var baseURL: String {
+        switch self {
+        case .`default`:
+            return "https://reqres.in"
+        }
+    }
+    
+    var path: String {
+        switch self {
+        case .`default`:
+            return "/api/users/2"
+        }
+    }
+}
