420d76643493efca3dc7174d0559b368d5433be9
Donguk Kim <ehddnr73@naver.com>
2023-03-02 19:24:33 +0900

    DELETE method를 테스트하는 메서드 정의 및 DefaultNetworkService 리팩토링

--- NetworkInfrastructure/NetworkInfrastructure/Infrastructure/NetworkService.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Infrastructure/NetworkService.swift
@@ -12,22 +12,80 @@ public protocol NetworkService: AnyObject {
     
     func requestGet(httpMethod: HttpMethodType, id: String, completion: @escaping CompletionHandler)
     func requestPost(httpMethod: HttpMethodType, user: UserToPost, completion: @escaping CompletionHandler)
+    func requestDelete(httpMethod: HttpMethodType, id: String, completion: @escaping (Result<Bool, Error>) -> Void)
 }
 
 public final class DefaultNetworkService: NetworkService {
+    private func makeRequest(request: URLRequest, completion: @escaping CompletionHandler) {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data {
+                completion(.success(data))
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+// MARK: - GET
+extension DefaultNetworkService {
     public func requestGet(httpMethod: HttpMethodType, id: String, completion: @escaping CompletionHandler) {
         let endPoint = EndPoint.default
-        let request = prepareRequestGet(endPoint, httpMethod, id)
+        let request = prepareGetRequest(endPoint, httpMethod, id)
         makeRequest(request: request, completion: completion)
     }
     
+    // MARK: - Private
+    private func prepareGetRequest(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String) -> URLRequest {
+        var urlComponents = URLComponents(string: endPoint.baseURL)
+        urlComponents?.path = endPoint.path
+        urlComponents?.path += "/"
+        urlComponents?.path += id
+        var request = URLRequest(url: urlComponents!.url!)
+        request.httpMethod = httpMethod.rawValue
+        
+        return request
+    }
+}
+
+// MARK: - POST
+extension DefaultNetworkService {
     public func requestPost(httpMethod: HttpMethodType, user: UserToPost, completion: @escaping CompletionHandler) {
         let endPoint = EndPoint.default
-        let request = prepareRequestPost(endPoint, httpMethod, user)
+        let request = preparePostRequest(endPoint, httpMethod, user)
         makeRequest(request: request, completion: completion)
     }
     
-    private func prepareRequestGet(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String) -> URLRequest {
+    // MARK: - Private
+    private func preparePostRequest<BodyType: Encodable>(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ body: BodyType) -> URLRequest {
+        var urlComponents = URLComponents(string: endPoint.baseURL)
+        urlComponents?.path = endPoint.path
+        var request = URLRequest(url: urlComponents!.url!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = httpMethod.rawValue
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        return request
+    }
+}
+
+// MARK: - PUT
+extension DefaultNetworkService {
+    
+}
+
+// MARK: - DELETE
+extension DefaultNetworkService {
+    public func requestDelete(httpMethod: HttpMethodType, id: String, completion: @escaping (Result<Bool, Error>) -> Void) {
+        let endPoint = EndPoint.default
+        let request = prepareDeleteRequest(endPoint, httpMethod, id)
+        makeDeleteRequest(request: request, completion: completion)
+    }
+    
+    // MARK: - Private
+    private func prepareDeleteRequest(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String) -> URLRequest {
         var urlComponents = URLComponents(string: endPoint.baseURL)
         urlComponents?.path = endPoint.path
         urlComponents?.path += "/"
@@ -38,23 +96,18 @@ public final class DefaultNetworkService: NetworkService {
         return request
     }
     
-    private func prepareRequestPost<BodyType: Encodable>(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ body: BodyType) -> URLRequest {
-        var urlComponents = URLComponents(string: endPoint.baseURL)
-        urlComponents?.path = endPoint.path
-        var request = URLRequest(url: urlComponents!.url!)
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpMethod = httpMethod.rawValue
-        request.httpBody = try? JSONEncoder().encode(body)
-        
-        return request
-    }
-    
-    private func makeRequest(request: URLRequest, completion: @escaping CompletionHandler) {
+    private func makeDeleteRequest(request: URLRequest, completion: @escaping (Result<Bool, Error>) -> Void) {
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 completion(.failure(error))
-            } else if let data = data {
-                completion(.success(data))
+                return
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                print("Status code: \(response.statusCode)")
+                if response.statusCode == 204 {
+                    completion(.success(true))
+                }
             }
         }
         
