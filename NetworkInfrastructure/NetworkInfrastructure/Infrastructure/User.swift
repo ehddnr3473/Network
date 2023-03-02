0439f3f0a37e84051df5bf3e3a60bb6ac62b4cb7
Donguk Kim <ehddnr73@naver.com>
2023-03-02 16:07:05 +0900

    User, Codable struct 정의

---
+++ NetworkInfrastructure/NetworkInfrastructure/Infrastructure/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  NetworkInfrastructure
+//
+//  Created by 김동욱 on 2023/03/02.
+//
+
+import Foundation
+
+public struct UserResult: Codable {
+    let data: User
+    
+    public struct User: Codable {
+        let id: Int
+        let email: String
+        let firstName: String
+        let lastName: String
+        let avatar: String
+        
+        enum CodingKeys: String, CodingKey {
+            case id
+            case email
+            case firstName = "first_name"
+            case lastName = "last_name"
+            case avatar
+        }
+    }
+}
