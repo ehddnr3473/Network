81bb1c408d5c9ebb29caa71a98fc53e2e276ca4a
Donguk Kim <ehddnr73@naver.com>
2023-03-02 21:09:15 +0900

    UserToPut Encodable struct 정의

---
+++ NetworkInfrastructure/NetworkInfrastructure/Infrastructure/Entities/UserToPut.swift
@@ -0,0 +1,13 @@
+//
+//  UserToPut.swift
+//  NetworkInfrastructure
+//
+//  Created by 김동욱 on 2023/03/02.
+//
+
+import Foundation
+
+public struct UserToPut: Encodable {
+    let name: String
+    let job: String
+}
