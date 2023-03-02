8a3dd5f093c86557344dd3f54d8a79836e3edfb0
Donguk Kim <ehddnr73@naver.com>
2023-03-02 21:08:47 +0900

    PUT method 테스트를 위한 메서드 정의

--- NetworkInfrastructure/NetworkInfrastructure/Infrastructure/NetworkService.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Infrastructure/NetworkService.swift
@@ -12,10 +12,24 @@ public protocol NetworkService: AnyObject {
     
     func requestGet(httpMethod: HttpMethodType, id: String, completion: @escaping CompletionHandler)
     func requestPost(httpMethod: HttpMethodType, user: UserToPost, completion: @escaping CompletionHandler)
+    func requestPut(httpMethod: HttpMethodType, id: String, user: UserToPut, completion: @escaping CompletionHandler)
     func requestDelete(httpMethod: HttpMethodType, id: String, completion: @escaping (Result<Bool, Error>) -> Void)
 }
 
 public final class DefaultNetworkService: NetworkService {
+    // MARK: - Private
+    private func prepareRequest(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String) -> URLRequest {
+        var urlComponents = URLComponents(string: endPoint.baseURL)
+        urlComponents?.path = endPoint.path
+        urlComponents?.path += "/"
+        urlComponents?.path += id
+        
+        var request = URLRequest(url: urlComponents!.url!)
+        request.httpMethod = httpMethod.rawValue
+        
+        return request
+    }
+    
     private func makeRequest(request: URLRequest, completion: @escaping CompletionHandler) {
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
@@ -33,20 +47,8 @@ public final class DefaultNetworkService: NetworkService {
 extension DefaultNetworkService {
     public func requestGet(httpMethod: HttpMethodType, id: String, completion: @escaping CompletionHandler) {
         let endPoint = EndPoint.default
-        let request = prepareGetRequest(endPoint, httpMethod, id)
+        let request = prepareRequest(endPoint, httpMethod, id)
         makeRequest(request: request, completion: completion)
-    }
-    
-    // MARK: - Private
-    private func prepareGetRequest(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String) -> URLRequest {
-        var urlComponents = URLComponents(string: endPoint.baseURL)
-        urlComponents?.path = endPoint.path
-        urlComponents?.path += "/"
-        urlComponents?.path += id
-        var request = URLRequest(url: urlComponents!.url!)
-        request.httpMethod = httpMethod.rawValue
-        
-        return request
     }
 }
 
@@ -62,6 +64,7 @@ extension DefaultNetworkService {
     private func preparePostRequest<BodyType: Encodable>(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ body: BodyType) -> URLRequest {
         var urlComponents = URLComponents(string: endPoint.baseURL)
         urlComponents?.path = endPoint.path
+        
         var request = URLRequest(url: urlComponents!.url!)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = httpMethod.rawValue
@@ -73,29 +76,56 @@ extension DefaultNetworkService {
 
 // MARK: - PUT
 extension DefaultNetworkService {
+    public func requestPut(httpMethod: HttpMethodType, id: String, user: UserToPut, completion: @escaping CompletionHandler) {
+        let endPoint = EndPoint.default
+        let request = preparePutRequest(endPoint, httpMethod, id, user)
+        makeRequest(request: request, completion: completion)
+    }
     
+    // MARK: - Private
+    private func preparePutRequest<BodyType: Encodable>(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String, _ body: BodyType) -> URLRequest {
+        var urlComponents = URLComponents(string: endPoint.baseURL)
+        urlComponents?.path = endPoint.path
+        urlComponents?.path += "/"
+        urlComponents?.path += id
+        
+        var request = URLRequest(url: urlComponents!.url!)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = httpMethod.rawValue
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        return request
+    }
+    
+    // PUT 메서드에 응답 데이터가 없는 경우
+    private func makePutRequest(request: URLRequest, completion: @escaping (Result<Bool, Error>) -> Void) {
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            
+            if let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    completion(.success(true))
+                } else {
+                    completion(.success(false))
+                }
+            }
+        }
+        
+        task.resume()
+    }
 }
 
 // MARK: - DELETE
 extension DefaultNetworkService {
     public func requestDelete(httpMethod: HttpMethodType, id: String, completion: @escaping (Result<Bool, Error>) -> Void) {
         let endPoint = EndPoint.default
-        let request = prepareDeleteRequest(endPoint, httpMethod, id)
+        let request = prepareRequest(endPoint, httpMethod, id)
         makeDeleteRequest(request: request, completion: completion)
     }
     
     // MARK: - Private
-    private func prepareDeleteRequest(_ endPoint: EndPoint, _ httpMethod: HttpMethodType, _ id: String) -> URLRequest {
-        var urlComponents = URLComponents(string: endPoint.baseURL)
-        urlComponents?.path = endPoint.path
-        urlComponents?.path += "/"
-        urlComponents?.path += id
-        var request = URLRequest(url: urlComponents!.url!)
-        request.httpMethod = httpMethod.rawValue
-        
-        return request
-    }
-    
     private func makeDeleteRequest(request: URLRequest, completion: @escaping (Result<Bool, Error>) -> Void) {
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
