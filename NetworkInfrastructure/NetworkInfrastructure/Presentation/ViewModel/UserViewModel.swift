b7038eebfa5f187677537a8058e93225ba7c8813
Donguk Kim <ehddnr73@naver.com>
2023-03-02 18:35:13 +0900

    POST 테스트를 위한 메서드 정의

--- NetworkInfrastructure/NetworkInfrastructure/Presentation/ViewModel/UserViewModel.swift
+++ NetworkInfrastructure/NetworkInfrastructure/Presentation/ViewModel/UserViewModel.swift
@@ -10,13 +10,16 @@ import Foundation
 final class UserViewModel: ObservableObject {
     private let networkService: NetworkService
     @Published var user: UserResult.User?
+    @Published var postedUserResult: PostedUserResult?
     
     init(networkService: NetworkService) {
         self.networkService = networkService
     }
     
-    func getUserButtonTapped() {
-        networkService.request(httpMethod: .get) { result in
+    func getUserButtonTapped(id: String) {
+        guard Int(id) != nil else { return }
+        
+        networkService.requestGet(httpMethod: .get, id: id) { result in
             switch result {
             case .success(let data):
                 do {
@@ -34,6 +37,20 @@ final class UserViewModel: ObservableObject {
     }
     
     func postUserButtonTapped(name: String, job: String) {
-        
+        networkService.requestPost(httpMethod: .post, user: UserToPost(name: name, job: job)) { result in
+            switch result {
+            case .success(let data):
+                do {
+                    let postedUserResult = try JSONDecoder().decode(PostedUserResult.self, from: data)
+                    DispatchQueue.main.async {
+                        self.postedUserResult = postedUserResult
+                    }
+                } catch(let error) {
+                    print(String(describing: error))
+                }
+            case .failure(let error):
+                print(String(describing: error))
+            }
+        }
     }
 }
